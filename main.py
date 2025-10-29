b5c3404e41087600fcda2c5391ab9fc153a85256
gordonzev914-lang <gordonzev914@gmail.com>
2025-10-29 12:25:30 +0200

    own that works

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ options=["1.add a task",
          "2.show all tasks",
          "3.exit"]
 def main(tasks,options):
-    condition=True
-    while  condition is True:
+    get_out = True
+    while  get_out == True:
+        
 
-        menu=utils.get_user_choice(options)
+        choice=utils.get_user_choice(options)
+        
+        if choice=="1" or choice=="Add a task":
+            new_task=input("enter a task")
+            task_adder=utils.add_task(tasks,new_task)
+        elif choice=='2' or choice=="show_all_tasks":
+            task_shower=utils.show_all_tasks(tasks)
+        elif choice=='3' or choice=="leave":
+            get_out=utils.exit()
 
-        The_selected_option=utils.callis_a_function_by_choice(menu,task_adder,task_shower,exit,tasks)
+        
 
-        task_shower=utils.show_all_tasks()
-
-        new_task = input('enter task')
-        task_adder=utils.add_task(tasks,new_task)
-
-        get_out=utils.exit(condition)
+        
+print(main(todo,options))
+        
 
 
 
--- utils.py
+++ utils.py
@@ -5,15 +5,7 @@ def get_user_choice(options):
     choice=input()
     return choice
 
-def callis_a_function_by_choice(choice,task_adder,task_shower,exit,tasks,condition):
-    
-    if choice=="1" or choice=="Add a task":
-        
-        return task_adder()
-    elif choice=='2' or choice=="show_all_tasks":
-        return task_shower(tasks)
-    elif choice=='3' or choice=="leave":
-        return exit(condition)
+
     
 
 def show_all_tasks(tasks: list):
@@ -23,12 +15,12 @@ def show_all_tasks(tasks: list):
     print(tasks)
 
 
-# new_task=input("enter yoer task")    
+    
 
 def add_task(tasks:list,new_task:str):
     return tasks.append(new_task)
 
-def exit(condition):
-    return condition is False
+def exit():
+    return  False
     
 
